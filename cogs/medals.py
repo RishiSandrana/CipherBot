904f191ce545ad08636c439bb636ef191c7f7127
Rishi <44793266+RishiSandrana@users.noreply.github.com>
2024-10-16 00:05:25 -0500

    Update medals.py

--- cogs/medals.py
+++ cogs/medals.py
@@ -15,20 +15,32 @@ class medals(commands.Cog):
         discord.app_commands.Choice(name="Silver", value="Silver"),
         discord.app_commands.Choice(name="Bronze", value="Bronze")
     ])
-    async def medals(self, interaction: discord.Interaction, type: str, username: str = ''):
+    async def medals(self, interaction: discord.Interaction, type: str = '', username: str = ''):
         await interaction.response.defer()
 
         result_text = await self.get_medals_data(type, username)
 
         place_emoji = ""
+        embed_title = ""
+
         if type == "Gold":
             place_emoji = "🥇"
         elif type == "Silver":
             place_emoji = "🥈"
         elif type == "Bronze":
             place_emoji = "🥉"
+        else:
+            place_emoji = "🏅"
 
-        embed_title = f"{place_emoji} {username}'s {type} Medals" if username else f"{place_emoji} {type} Leaderboard"
+        if type != '' and username != '':
+            embed_title = f"{place_emoji} {username}'s {type} Medals"
+        elif type != '' and username == '':
+            embed_title = f"{place_emoji} {type} Leaderboard"
+        elif type == '' and username == '':
+            embed_title = f"{place_emoji} Total Medals Leaderboard"
+        elif type == '' and username != '':
+            embed_title = f"{place_emoji} {username}'s Total Medals"
+
         embed = discord.Embed(title=embed_title, description=result_text, color=0x1ba300)
         embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
         await interaction.followup.send(embed=embed)
@@ -170,6 +182,51 @@ class medals(commands.Cog):
 
                     text += f"[{badgeGame}: {badgeName}]({gameURL}) 🥉\n"
 
+        elif type == '' and username == '':
+            cursor.execute("""
+                    SELECT username, 
+                        SUM(CASE WHEN place = 1 THEN 1 ELSE 0 END) as gold_count,
+                        SUM(CASE WHEN place = 2 THEN 1 ELSE 0 END) as silver_count,
+                        SUM(CASE WHEN place = 3 THEN 1 ELSE 0 END) as bronze_count
+                    FROM medals
+                    GROUP BY username
+                    ORDER BY (gold_count + silver_count + bronze_count) DESC
+                """)
+
+            results = cursor.fetchall()
+
+            count = 1
+            for row in results:
+                user, gold_count, silver_count, bronze_count = row
+                total_count = gold_count + silver_count + bronze_count
+                if count <= 3:
+                    text += f"__{count}. {user} - {total_count} Medals (🥇 {gold_count}, 🥈 {silver_count}, 🥉 {bronze_count})__\n"
+                else:
+                    text += f"{count}. {user} - {total_count} Medals (🥇 {gold_count}, 🥈 {silver_count}, 🥉 {bronze_count})\n"
+                count += 1
+
+                if count > 50:
+                    break
+
+        elif type == '' and username != '':
+            cursor.execute("""
+                SELECT 
+                    SUM(CASE WHEN place = 1 THEN 1 ELSE 0 END) as gold_count,
+                    SUM(CASE WHEN place = 2 THEN 1 ELSE 0 END) as silver_count,
+                    SUM(CASE WHEN place = 3 THEN 1 ELSE 0 END) as bronze_count
+                FROM medals
+                WHERE username = ?
+            """, (username,))
+
+            result = cursor.fetchone()
+            gold_count, silver_count, bronze_count = result
+
+            if gold_count is None:
+                text += f"{username} has no medals."
+            else:
+                total_count = gold_count + silver_count + bronze_count
+                text += f"{username} has a total of {total_count} Medals:\n🥇 {gold_count} Gold\n🥈 {silver_count} Silver\n🥉 {bronze_count} Bronze"
+
         connection.commit()
         connection.close()
 
@@ -177,201 +234,3 @@ class medals(commands.Cog):
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(medals(client))
-
-# import discord
-# import sqlite3
-# import requests
-# import asyncio
-# from discord import app_commands
-# from discord.ext import commands
-#
-# class medals(commands.Cog):
-#     def __init__(self, client: commands.Bot):
-#         self.client = client
-#
-#     @app_commands.command(name="medals", description="Find people with the most medals")
-#     @app_commands.describe(type="Select your scope")
-#     @app_commands.choices(type=[
-#         discord.app_commands.Choice(name="Gold", value="Gold"),
-#         discord.app_commands.Choice(name="Silver", value="Silver"),
-#         discord.app_commands.Choice(name="Bronze", value="Bronze")
-#     ])
-#     async def medals(self, interaction: discord.Interaction, type: str, username: str = ''):
-#         await interaction.response.defer()
-#         await asyncio.sleep(4)
-#
-#         connection = sqlite3.connect("medals.db")
-#         cursor = connection.cursor()
-#
-#         text = ""
-#
-#         if type == "Gold" and username == '':
-#             cursor.execute("""
-#                 SELECT username, COUNT(*) as gold_count
-#                 FROM medals
-#                 WHERE place = 1
-#                 GROUP BY username
-#                 ORDER BY gold_count DESC
-#             """)
-#
-#             results = cursor.fetchall()
-#
-#             count = 1
-#             for row in results:
-#                 username, gold_count = row
-#                 if count <= 3:
-#                     text += f"__{count}. {username} - {gold_count}__ \n"
-#                 else:
-#                     text += f"{count}. {username} - {gold_count} \n"
-#                 count += 1
-#
-#             connection.commit()
-#             connection.close()
-#
-#             embed = discord.Embed(title="🥇 Leaderboard", description=text, color=0x1ba300)
-#             embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
-#             await interaction.followup.send(embed=embed)
-#
-#         elif type == "Gold" and username != '':
-#             place = 1
-#             cursor.execute(f"""
-#                 SELECT *
-#                 FROM medals
-#                 WHERE username = ? AND place = ?
-#             """, (username, place))
-#
-#             results = cursor.fetchall()
-#
-#             for row in reversed(results):
-#                 badgeID = row[1]
-#
-#                 badgeName = requests.get(f"https://badges.roblox.com/v1/badges/{badgeID}").json()["name"]
-#                 badgeGame = requests.get(f"https://badges.roblox.com/v1/badges/{badgeID}").json()["awardingUniverse"][
-#                     "name"]
-#                 gameID = requests.get(f"https://badges.roblox.com/v1/badges/{badgeID}").json()["awardingUniverse"][
-#                     "rootPlaceId"]
-#                 gameURL = f"https://www.roblox.com/games/{gameID}/"
-#
-#                 text += f"[{badgeGame}: {badgeName}]({gameURL}) 🥇\n"
-#
-#             connection.commit()
-#             connection.close()
-#
-#             embed = discord.Embed(title = f"{username}'s Gold Medals", description=text, color=0x1ba300)
-#             embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
-#             await interaction.followup.send(embed=embed)
-#
-#         elif type == "Silver" and username == '':
-#             cursor.execute("""
-#                 SELECT username, COUNT(*) as silver_count
-#                 FROM medals
-#                 WHERE place = 2
-#                 GROUP BY username
-#                 ORDER BY silver_count DESC
-#             """)
-#
-#             results = cursor.fetchall()
-#
-#             count = 1
-#             for row in results:
-#                 username, silver_count = row
-#                 if count <= 3:
-#                     text += f"__{count}. {username} - {silver_count}__ \n"
-#                 else:
-#                     text += f"{count}. {username} - {silver_count} \n"
-#                 count += 1
-#
-#             connection.commit()
-#             connection.close()
-#
-#             embed = discord.Embed(title="🥈 Leaderboard", description=text, color=0x1ba300)
-#             embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
-#             await interaction.followup.send(embed=embed)
-#
-#         elif type == "Silver" and username != '':
-#             place = 2
-#             cursor.execute(f"""
-#                 SELECT *
-#                 FROM medals
-#                 WHERE username = ? AND place = ?
-#             """, (username, place))
-#
-#             results = cursor.fetchall()
-#
-#             for row in reversed(results):
-#                 badgeID = row[1]
-#
-#                 badgeName = requests.get(f"https://badges.roblox.com/v1/badges/{badgeID}").json()["name"]
-#                 badgeGame = requests.get(f"https://badges.roblox.com/v1/badges/{badgeID}").json()["awardingUniverse"][
-#                     "name"]
-#                 gameID = requests.get(f"https://badges.roblox.com/v1/badges/{badgeID}").json()["awardingUniverse"][
-#                     "rootPlaceId"]
-#                 gameURL = f"https://www.roblox.com/games/{gameID}/"
-#
-#                 text += f"[{badgeGame}: {badgeName}]({gameURL}) 🥈\n"
-#
-#             connection.commit()
-#             connection.close()
-#
-#             embed = discord.Embed(title = f"{username}'s Silver Medals", description=text, color=0x1ba300)
-#             embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
-#             await interaction.followup.send(embed=embed)
-#
-#         elif type == "Bronze" and username == '':
-#             cursor.execute("""
-#                 SELECT username, COUNT(*) as bronze_count
-#                 FROM medals
-#                 WHERE place = 3
-#                 GROUP BY username
-#                 ORDER BY bronze_count DESC
-#             """)
-#
-#             results = cursor.fetchall()
-#
-#             count = 1
-#             for row in results:
-#                 username, bronze_count = row
-#                 if count <= 3:
-#                     text += f"__{count}. {username} - {bronze_count}__ \n"
-#                 else:
-#                     text += f"{count}. {username} - {bronze_count} \n"
-#                 count += 1
-#
-#             connection.commit()
-#             connection.close()
-#
-#             embed = discord.Embed(title="🥉 Leaderboard", description=text, color=0x1ba300)
-#             embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
-#             await interaction.followup.send(embed=embed)
-#
-#         elif type == "Bronze" and username != '':
-#             place = 3
-#             cursor.execute(f"""
-#                 SELECT *
-#                 FROM medals
-#                 WHERE username = ? AND place = ?
-#             """, (username, place))
-#
-#             results = cursor.fetchall()
-#
-#             for row in reversed(results):
-#                 badgeID = row[1]
-#
-#                 badgeName = requests.get(f"https://badges.roblox.com/v1/badges/{badgeID}").json()["name"]
-#                 badgeGame = requests.get(f"https://badges.roblox.com/v1/badges/{badgeID}").json()["awardingUniverse"][
-#                     "name"]
-#                 gameID = requests.get(f"https://badges.roblox.com/v1/badges/{badgeID}").json()["awardingUniverse"][
-#                     "rootPlaceId"]
-#                 gameURL = f"https://www.roblox.com/games/{gameID}/"
-#
-#                 text += f"[{badgeGame}: {badgeName}]({gameURL}) 🥉\n"
-#
-#             connection.commit()
-#             connection.close()
-#
-#             embed = discord.Embed(title = f"{username}'s Bronze Medals", description=text, color=0x1ba300)
-#             embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
-#             await interaction.followup.send(embed=embed)
-#
-# async def setup(client: commands.Bot) -> None:
-#     await client.add_cog(medals(client))
