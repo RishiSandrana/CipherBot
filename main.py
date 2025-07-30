08bbd04690e547777349e2f2369e9b15a3c9eef1
Rishi <44793266+RishiSandrana@users.noreply.github.com>
2025-07-29 22:39:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -33,22 +33,6 @@ async def on_ready():
     # print(e)
     logger.exception("Error syncing commands: %s", e)
 
-@client.tree.command(name = "ship", description = "<3")
-async def ship(interaction: discord.Interaction):
-  randNumber = random.randint(1, 4)
-
-  if randNumber == 1:
-    await interaction.response.send_message("https://cdn.discordapp.com/attachments/901665700333097081/1066615229246615643/image.png")
-
-  elif randNumber == 2:
-    await interaction.response.send_message("https://cdn.discordapp.com/attachments/782641749574549544/1112649214615359530/Untitled425_20230529125014.png")
-
-  elif randNumber == 3:
-    await interaction.response.send_message("https://cdn.discordapp.com/attachments/773997636545544222/959319246855372840/image0.png")
-
-  elif randNumber == 4:
-    await interaction.response.send_message("https://cdn.discordapp.com/attachments/949016494308749392/1278204526813515776/Untitled120_20240828000758.png?ex=66f1927b&is=66f040fb&hm=a8d8aa9cbfcb09ae6f0df15f2d89acf11049c7681d9372495573e8820b259682&")
-
 @client.tree.command(name = "help", description = "Document with all the commands!")
 async def help(interaction: discord.Interaction):
   await interaction.response.send_message("https://docs.google.com/document/d/1jOs-9klLJfeVkBwajwTBeUbqumI_U8bP8oFuUKnNuKA/edit?usp=sharing")
